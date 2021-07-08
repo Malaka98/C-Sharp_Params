78d64da80783bb59b531945fb3bf3a7c65e5a4f5
Malaka98 <malaka.aruna14413@gmail.com>
2021-07-08 12:58:17 +0530

    Add project files.

---
+++ C-Sharp_Params/Display.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Params
+{
+    internal class Display
+    {
+        internal static void Show(params int[] val)
+        {
+            foreach(int x in val)
+            {
+                Console.Write(x + "\t");
+            }
+        }
+    }
+}
---
+++ C-Sharp_Params/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace C_Sharp_Params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Display.Show(1, 2, 3);
+            Console.WriteLine("");
+            Display.Show(1, 2, 3,4,5,6,7,8);
+            Console.WriteLine("");
+            Display.Show(1, 2, 3,4,5,6,7,8,9,10,11,12);
+            Console.WriteLine("");
+            Display.Show(1, 2, 3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+        }
+    }
+}
